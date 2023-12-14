6a54e579d522739dc38d27be1a931821820720d3
Vitalii Umanets <umanetsvitaliy@gmail.com>
2023-12-14 21:47:40 +0000

    Task15

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -3,7 +3,7 @@
 
 provider "registry.terraform.io/hashicorp/google" {
   version     = "4.52.0"
-  constraints = "4.52.0"
+  constraints = ">= 4.47.0, != 4.49.0, != 4.50.0, 4.52.0, < 6.0.0"
   hashes = [
     "h1:Nlb8C/liQ7fnT4sNXSjTGxTlwzw/w28K96kp3TI7DWo=",
     "zh:1e36ac7c1be43cbb1d05fe1e5497dd058d1d87e03f848fd200bb043b1d6883f9",
@@ -18,5 +18,24 @@ provider "registry.terraform.io/hashicorp/google" {
     "zh:bfd2d4cb2daba199db49a33ff507c808a3c94a657e4597b8bd6f2de51dc7a729",
     "zh:d9fd24b3d492013bca0021f29bfb670b76f01df5fd45f62c61eec53adac1b47d",
     "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.4.1"
+  hashes = [
+    "h1:FzraUapGrJoH3ZOWiUT2m6QpZAD+HmU+JmqZgM4/o2Y=",
+    "zh:244b445bf34ddbd167731cc6c6b95bbed231dc4493f8cc34bd6850cfe1f78528",
+    "zh:3c330bdb626123228a0d1b1daa6c741b4d5d484ab1c7ae5d2f48d4c9885cc5e9",
+    "zh:5ff5f9b791ddd7557e815449173f2db38d338e674d2d91800ac6e6d808de1d1d",
+    "zh:70206147104f4bf26ae67d730c995772f85bf23e28c2c2e7612c74f4dae3c46f",
+    "zh:75029676993accd6bef933c196b2fad51a9ec8a69a847dbbe96ec8ebf7926cdc",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7d48d5999fe1fcdae9295a7c3448ac1541f5a24c474bd82df6d4fa3732483f2b",
+    "zh:b766b38b027f0f84028244d1c2f990431a37d4fc3ac645962924554016507e77",
+    "zh:bfc7ad301dada204cf51c59d8bd6a9a87de5fddb42190b4d6ba157d6e08a1f10",
+    "zh:c902b527702a8c5e2c25a6637d07bbb1690cb6c1e63917a5f6dc460efd18d43f",
+    "zh:d68ae0e1070cf429c46586bc87580c3ed113f76241da2b6e4f1a8348126b3c46",
+    "zh:f4903fd89f7c92a346ae9e666c2d0b6884c4474ae109e9b4bd15e7efaa4bfc29",
   ]
 }
--- main.tf
+++ main.tf
@@ -1,50 +1,31 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.52.0"
+provider "google" {
+  # Configuration options
+  project = var.GOOGLE_PROJECT
+  region  = var.GOOGLE_REGION
+}
+
+resource "google_container_cluster" "this" {
+  name     = var.GKE_CLUSTER_NAME
+  location = var.GOOGLE_REGION
+
+  initial_node_count       = 1
+  remove_default_node_pool = true
+
+    workload_identity_config {
+    workload_pool = "${var.GOOGLE_PROJECT}.svc.id.goog"
+  }
+  node_config {
+        workload_metadata_config {
+      mode = "GKE_METADATA"
     }
   }
 }
 
-variable "GOOGLE_PROJECT" {
-  type        = string
-  description = "GCP project name"
-}
-
-variable "GOOGLE_REGION" {
-  type        = string
-  default     = "us-central1-c"
-  description = "GCP region to use"
-}
-
-
-variable "GKE_MACHINE_TYPE" {
-  type        = string
-  default     = "g1-small"
-  description = "Machine type"
-}
-
-variable "GKE_NUM_NODES" {
-  type        = number
-  default     = 2
-  description = "GKE nodes number"
-}
-
-resource "google_container_cluster" "demo" {
-  name     = "demo"
-  location = var.GOOGLE_REGION
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
-
-
-resource "google_container_node_pool" "main" {
-  name       = "main"
-  project    = google_container_cluster.demo.project
-  cluster    = google_container_cluster.demo.name
-  location   = google_container_cluster.demo.location
+resource "google_container_node_pool" "this" {
+  name       = var.GKE_POOL_NAME
+  project    = google_container_cluster.this.project
+  cluster    = google_container_cluster.this.name
+  location   = google_container_cluster.this.location
   node_count = var.GKE_NUM_NODES
 
   node_config {
@@ -52,23 +33,20 @@ resource "google_container_node_pool" "main" {
   }
 }
 
-data "google_compute_instance_group" "node_instance_groups" {
-  self_link = google_container_cluster.demo.node_pool[0].managed_instance_group_urls[0]
+module "gke_auth" {
+  depends_on = [
+    google_container_cluster.this
+  ]
+  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version              = ">= 24.0.0"
+  project_id           = var.GOOGLE_PROJECT
+  cluster_name         = google_container_cluster.this.name
+  location             = var.GOOGLE_REGION
 }
 
-data "google_compute_instance" "nodes" {
-  for_each = toset(data.google_compute_instance_group.node_instance_groups.instances[*])
-  self_link = each.key
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "${path.module}/kubeconfig"
+  file_permission = "0400"
 }
-
-output "node_ip" {
-  value = [for node in data.google_compute_instance.nodes : node.network_interface[0].access_config[0].nat_ip]
-}
-
-provider "google" {
-  # Configuration options
-  project = var.GOOGLE_PROJECT
-  region  = var.GOOGLE_REGION
-}
-
 
