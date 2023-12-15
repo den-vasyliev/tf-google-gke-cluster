ec1238f61a50f2acb006f26ac77da90131b8afb6
yuandrk <yurii.andriuk@gmail.com>
2023-12-15 21:07:34 +0000

    Update GKE_NUM_NODES default value to 4

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "GKE_MACHINE_TYPE" {
 
 variable "GKE_NUM_NODES" {
   type        = number
-  default     = 2
+  default     = 3 # updated from 2 to 4 
   description = "GKE nodes number"
 }
 
