3d4276e44c1cd2bdb4a5f34667d242bd9c674ba6
artur-titov <fop.titov.av@gmail.com>
2024-08-29 23:06:43 +0200

    test tf update

--- main.tf
+++ main.tf
@@ -1,52 +1,45 @@
 provider "google" {
-  # Configuration options
-  project = var.GOOGLE_PROJECT
-  region  = var.GOOGLE_REGION
+  region = "us-central1"
+  project = "test"
 }
 
-resource "google_container_cluster" "this" {
-  name     = var.GKE_CLUSTER_NAME
-  location = var.GOOGLE_REGION
+resource "google_compute_instance" "my_instance" {
+  zone = "us-central1-a"
+  name = "test"
 
-  initial_node_count       = 1
-  remove_default_node_pool = true
-
-    workload_identity_config {
-    workload_pool = "${var.GOOGLE_PROJECT}.svc.id.goog"
+  machine_type = "n1-standard-16" # <<<<<<<<<< Try changing this to n1-standard-32 to compare the costs
+  network_interface {
+    network = "default"
+    access_config {}
   }
-  node_config {
-        workload_metadata_config {
-      mode = "GKE_METADATA"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
     }
   }
-}
 
-resource "google_container_node_pool" "this" {
-  name       = var.GKE_POOL_NAME
-  project    = google_container_cluster.this.project
-  cluster    = google_container_cluster.this.name
-  location   = google_container_cluster.this.location
-  node_count = var.GKE_NUM_NODES
+  scheduling {
+    preemptible = true
+  }
 
-  node_config {
-    machine_type = var.GKE_MACHINE_TYPE
+  guest_accelerator {
+    type = "nvidia-tesla-t4" # <<<<<<<<<< Try changing this to nvidia-tesla-p4 to compare the costs
+    count = 4
+  }
+
+  labels = {
+    environment = "production"
+    service = "web-app"
   }
 }
 
-module "gke_auth" {
-  depends_on = [
-    google_container_cluster.this
-  ]
-  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  version              = ">= 24.0.0"
-  project_id           = var.GOOGLE_PROJECT
-  cluster_name         = google_container_cluster.this.name
-  location             = var.GOOGLE_REGION
-}
+resource "google_cloudfunctions_function" "my_function" {
+  runtime = "nodejs20"
+  name = "test"
+  available_memory_mb = 512
 
-resource "local_file" "kubeconfig" {
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "${path.module}/kubeconfig"
-  file_permission = "0400"
+  labels = {
+    environment = "Prod"
+  }
 }
-
